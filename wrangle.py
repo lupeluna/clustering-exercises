1557496a5b9bd7b28a6e5a571e1178e16b7023c9
lupeluna <80718680+lupeluna@users.noreply.github.com>
2021-06-18 14:05:14 -0500

    wrangle for zillow

---
+++ wrangle.py
@@ -0,0 +1,148 @@
+import pandas as pd
+import numpy as np
+import os
+# acquire
+from env import host, user, password
+from pydataset import data
+from sklearn.preprocessing import MinMaxScaler
+# turn off pink warning boxes
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.model_selection import train_test_split
+
+
+###############acquire#################
+
+
+def get_connection(database, user=user, host=host, password=password):
+    '''get URL with user, host, and password from env '''
+    
+    return f"mysql+pymysql://{user}:{password}@{host}/{database}"
+    
+    
+def cache_sql_data(df, database):
+    '''write dataframe to csv with title database_query.csv'''
+    
+    df.to_csv(f'{database}_query.csv',index = False)
+        
+
+def get_sql_data(database,query):
+    ''' check if csv exists for the queried database
+        if it does read from the csv
+        if it does not create the csv then read from the csv  
+    '''
+    
+    if os.path.isfile(f'{database}_query.csv') == False:   # check for the file
+        
+        df = pd.read_sql(query, get_connection(database))  # create file 
+        
+        cache_sql_data(df, database) # cache file
+        
+    return pd.read_csv(f'{database}_query.csv') # return contents of file
+
+
+def get_zillow_data():
+    ''' acquire zillow data'''
+    
+    query = '''
+
+    select * 
+    from predictions_2017
+
+    left join properties_2017 using(parcelid)
+    left join airconditioningtype using(airconditioningtypeid)
+    left join architecturalstyletype using(architecturalstyletypeid)
+    left join buildingclasstype using(buildingclasstypeid)
+    left join heatingorsystemtype using(heatingorsystemtypeid)
+    left join propertylandusetype using(propertylandusetypeid)
+    left join storytype using(storytypeid)
+    left join typeconstructiontype using(typeconstructiontypeid)
+
+    where latitude is not null
+
+    and longitude is not null
+
+    '''
+
+    database = "zillow"
+    
+    df = get_sql_data(database,query) # create/read csv for query
+    
+    df = df.sort_values('transactiondate').drop_duplicates('parcelid',keep='last') # drop duplicate parcelids keeping the latest
+    
+    return df 
+
+#################################prepare###############################
+
+def remove_outliers(df, k, col_list):
+    ''' remove outliers from a list of columns in a dataframe 
+        and return that dataframe
+    '''
+    
+    for col in col_list:
+
+        q1, q3 = df[f'{col}'].quantile([.25, .75])  # get quartiles
+        
+        iqr = q3 - q1   # calculate interquartile range
+        
+        upper_bound = q3 + k * iqr   # get upper bound
+        lower_bound = q1 - k * iqr   # get lower bound
+
+        # return dataframe without outliers
+        
+        return df[(df[f'{col}'] > lower_bound) & (df[f'{col}'] < upper_bound)]  
+    
+
+def handle_missing_values(df, prop_required_column = .5, prop_required_row = .5):
+    ''' 
+        take in a dataframe and a proportion for columns and rows
+        return dataframe with columns and rows not meeting proportions dropped
+    '''
+    col_thresh = int(round(prop_required_column*df.shape[0],0)) # calc column threshold
+    
+    df.dropna(axis=1, thresh=col_thresh, inplace=True) # drop columns with non-nulls less than threshold
+    
+    row_thresh = int(round(prop_required_row*df.shape[1],0))  # calc row threshhold
+    
+    df.dropna(axis=0, thresh=row_thresh, inplace=True) # drop columns with non-nulls less than threshold
+    
+    return df    
+    
+    
+def impute(df, my_strategy, column_list):
+    ''' take in a df strategy and cloumn list
+        return df with listed columns imputed using input stratagy
+    '''
+        
+    imputer = SimpleImputer(strategy=my_strategy)  # build imputer
+
+    df[column_list] = imputer.fit_transform(df[column_list]) # fit/transform selected columns
+
+    return df
+
+def prepare_zillow(df):
+    ''' Prepare Zillow Data'''
+    
+    # Restrict propertylandusedesc to those of single unit
+    df = df[(df.propertylandusedesc == 'Single Family Residential') |
+          (df.propertylandusedesc == 'Mobile Home') |
+          (df.propertylandusedesc == 'Manufactured, Modular, Prefabricated Homes') |
+          (df.propertylandusedesc == 'Townhouse')]
+    
+    # remove outliers in bed count, bath count, and area to better target single unit properties
+    df = remove_outliers(df, 1.5, ['calculatedfinishedsquarefeet', 'bedroomcnt', 'bathroomcnt'])
+    
+    # dropping cols/rows where more than half of the values are null
+    df = handle_missing_values(df, prop_required_column = .5, prop_required_row = .5)
+    
+    # dropping the columns with 17K missing values too much to fill/impute/drop rows
+    df = df.drop(columns=['heatingorsystemtypeid', 'buildingqualitytypeid', 'propertyzoningdesc', 'unitcnt', 'heatingorsystemdesc'])
+    
+    # imputing descreet columns with most frequent value
+    df = impute(df, 'most_frequent', ['calculatedbathnbr', 'fullbathcnt', 'regionidcity', 'regionidzip', 'yearbuilt', 'censustractandblock'])
+    
+    # imputing continuous columns with median value
+    df = impute(df, 'median', ['finishedsquarefeet12', 'lotsizesquarefeet', 'structuretaxvaluedollarcnt', 'taxvaluedollarcnt', 'landtaxvaluedollarcnt', 'taxamount'])
+    
+    return df
